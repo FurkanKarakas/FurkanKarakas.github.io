fff3c3deec5ab3ec3cc38231eb68ed6ca0537e2b
Furkan Karakas <furkan.karakas@epfl.ch>
2021-06-20 20:29:53 +0200

    add if statement

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,9 @@
 // Custom JavaScript file to add interactivity to the page elements
 const lastMod = document.getElementById("last-modified");
-const [date, time] = document.lastModified.split(" ");
-const [month, day, year] = date.split("/");
-lastMod.innerHTML = `Last modified on ${
-  day + "." + month + "." + year
-} at ${time}`;
+if (lastMod) {
+  const [date, time] = document.lastModified.split(" ");
+  const [month, day, year] = date.split("/");
+  lastMod.innerHTML = `Last modified on ${
+    day + "." + month + "." + year
+  } at ${time}`;
+}
